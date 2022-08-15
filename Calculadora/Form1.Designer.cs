f9d0a21b12232f87ebecda1ab668d913e78e0c50
BrunoSdAlencar <brunosda.work@gmail.com>
2022-08-15 08:12:21 -0300

    calculadora

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -25,13 +25,283 @@
         /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.btsub = new System.Windows.Forms.Button();
+            this.btsom = new System.Windows.Forms.Button();
+            this.btdiv = new System.Windows.Forms.Button();
+            this.btmult = new System.Windows.Forms.Button();
+            this.btlimpar = new System.Windows.Forms.Button();
+            this.btigual = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.btvir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtvalor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt7
+            // 
+            this.bt7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt7.Location = new System.Drawing.Point(25, 59);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(106, 55);
+            this.bt7.TabIndex = 0;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt8.Location = new System.Drawing.Point(137, 59);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(106, 55);
+            this.bt8.TabIndex = 1;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt9.Location = new System.Drawing.Point(249, 59);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(106, 55);
+            this.bt9.TabIndex = 2;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt6.Location = new System.Drawing.Point(249, 120);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(106, 55);
+            this.bt6.TabIndex = 5;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt5.Location = new System.Drawing.Point(137, 120);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(106, 55);
+            this.bt5.TabIndex = 4;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt4.Location = new System.Drawing.Point(25, 120);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(106, 55);
+            this.bt4.TabIndex = 3;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt3.Location = new System.Drawing.Point(249, 181);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(106, 55);
+            this.bt3.TabIndex = 8;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt2.Location = new System.Drawing.Point(137, 181);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(106, 55);
+            this.bt2.TabIndex = 7;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt1.Location = new System.Drawing.Point(25, 181);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(106, 55);
+            this.bt1.TabIndex = 6;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // btsub
+            // 
+            this.btsub.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btsub.Location = new System.Drawing.Point(473, 181);
+            this.btsub.Name = "btsub";
+            this.btsub.Size = new System.Drawing.Size(106, 55);
+            this.btsub.TabIndex = 14;
+            this.btsub.Text = "-";
+            this.btsub.UseVisualStyleBackColor = true;
+            // 
+            // btsom
+            // 
+            this.btsom.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btsom.Location = new System.Drawing.Point(361, 181);
+            this.btsom.Name = "btsom";
+            this.btsom.Size = new System.Drawing.Size(106, 55);
+            this.btsom.TabIndex = 13;
+            this.btsom.Text = "+";
+            this.btsom.UseVisualStyleBackColor = true;
+            // 
+            // btdiv
+            // 
+            this.btdiv.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btdiv.Location = new System.Drawing.Point(473, 120);
+            this.btdiv.Name = "btdiv";
+            this.btdiv.Size = new System.Drawing.Size(106, 55);
+            this.btdiv.TabIndex = 12;
+            this.btdiv.Text = "/";
+            this.btdiv.UseVisualStyleBackColor = true;
+            // 
+            // btmult
+            // 
+            this.btmult.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btmult.Location = new System.Drawing.Point(361, 120);
+            this.btmult.Name = "btmult";
+            this.btmult.Size = new System.Drawing.Size(106, 55);
+            this.btmult.TabIndex = 11;
+            this.btmult.Text = "*";
+            this.btmult.UseVisualStyleBackColor = true;
+            // 
+            // btlimpar
+            // 
+            this.btlimpar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btlimpar.Location = new System.Drawing.Point(361, 59);
+            this.btlimpar.Name = "btlimpar";
+            this.btlimpar.Size = new System.Drawing.Size(218, 55);
+            this.btlimpar.TabIndex = 9;
+            this.btlimpar.Text = "C";
+            this.btlimpar.UseVisualStyleBackColor = true;
+            this.btlimpar.Click += new System.EventHandler(this.btlimpar_Click);
+            // 
+            // btigual
+            // 
+            this.btigual.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btigual.Location = new System.Drawing.Point(361, 242);
+            this.btigual.Name = "btigual";
+            this.btigual.Size = new System.Drawing.Size(218, 55);
+            this.btigual.TabIndex = 15;
+            this.btigual.Text = "=";
+            this.btigual.UseVisualStyleBackColor = true;
+            this.btigual.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt0.Location = new System.Drawing.Point(25, 242);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(218, 55);
+            this.bt0.TabIndex = 16;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // btvir
+            // 
+            this.btvir.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btvir.Location = new System.Drawing.Point(249, 242);
+            this.btvir.Name = "btvir";
+            this.btvir.Size = new System.Drawing.Size(106, 55);
+            this.btvir.TabIndex = 17;
+            this.btvir.Text = ",";
+            this.btvir.UseVisualStyleBackColor = true;
+            this.btvir.Click += new System.EventHandler(this.btnNumerador_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 40);
+            this.textBox1.TabIndex = 18;
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.AutoSize = true;
+            this.txtvalor.BackColor = System.Drawing.Color.White;
+            this.txtvalor.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalor.Location = new System.Drawing.Point(30, 24);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(0, 24);
+            this.txtvalor.TabIndex = 19;
+            this.txtvalor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(597, 305);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btvir);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.btigual);
+            this.Controls.Add(this.btsub);
+            this.Controls.Add(this.btsom);
+            this.Controls.Add(this.btdiv);
+            this.Controls.Add(this.btmult);
+            this.Controls.Add(this.btlimpar);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button btsub;
+        private System.Windows.Forms.Button btsom;
+        private System.Windows.Forms.Button btdiv;
+        private System.Windows.Forms.Button btmult;
+        private System.Windows.Forms.Button btlimpar;
+        private System.Windows.Forms.Button btigual;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button btvir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtvalor;
     }
 }
 
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,8 +12,21 @@ namespace Calculadora
 {
     public partial class Form1 : Form
     {
+        string operador;
+        int a = 0;
+        bool validar = false;
+
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btnNumerador_Click(object sender, EventArgs e) {
+            Button bt = (Button)sender;
+            txtvalor.Text = txtvalor.Text + bt.Text;
+        }
+
+        private void btlimpar_Click(object sender, EventArgs e) {
+            txtvalor.Text = "";
         }
     }
 }
